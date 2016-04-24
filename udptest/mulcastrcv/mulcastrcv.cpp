0933c431b6a31274650ccda6d1081afd6196ae4d
alioxp <alioxp@gmail.com>
2016-04-24 23:23:43 +0800

    speed test

--- udptest/mulcastrcv/mulcastrcv.cpp
+++ udptest/mulcastrcv/mulcastrcv.cpp
@@ -8,7 +8,8 @@
 #define MCAST_ADDR "224.0.1.88" /*一个局部连接多播地址，路由器不进行转发*/
 #define MCAST_DATA "BROADCAST TEST DATA"  // 多播内容
 #define MCAST_INTERVAL 5  //发送时间间隔
-#define BUFF_SIZE 256   //接收缓冲区大小
+//#define BUFF_SIZE 256   //接收缓冲区大小
+#define UDP_BIG_PKG (1024*32)
 
 int _tmain(int argc, _TCHAR* argv[])
 {
@@ -22,6 +23,12 @@ int _tmain(int argc, _TCHAR* argv[])
 
 	bool bReuse = true;
 	::setsockopt(s,SOL_SOCKET, SO_REUSEADDR,(char*)&bReuse,sizeof(bool));
+
+	int nRecvBuf=UDP_BIG_PKG;//设置为32K
+	setsockopt (s,SOL_SOCKET,SO_RCVBUF,(const char*)&nRecvBuf,sizeof(int));
+	//发送缓冲区
+	int nSendBuf=UDP_BIG_PKG;//设置为32K
+	setsockopt (s,SOL_SOCKET,SO_SNDBUF,(const char*)&nSendBuf,sizeof(int));
 
 	SOCKADDR_IN sin;
 	sin.sin_family = AF_INET;
@@ -54,28 +61,31 @@ int _tmain(int argc, _TCHAR* argv[])
 		return -4;
 	}
 
+	int count = 0;
 	printf("开始接收30000");
 	SOCKADDR_IN addrRemote;
 	int nLen = sizeof(addrRemote);
-	char sz[256];
+	char sz[1024*64];
+	
 	while(1)
 	{
-		int nRet = ::recvfrom(s,sz,256,0,(sockaddr*)&addrRemote,&nLen);
+		int nRet = ::recvfrom(s,sz,UDP_BIG_PKG*2,0,(sockaddr*)&addrRemote,&nLen);
 		if(nRet>0)
 		{
-			sz[nRet]= '\0';
-			printf(sz);
+			//sz[nRet]= '\0';
+			printf("count:%d src:%d\n",count,sz[0]);
 		}
 
-		char sz[] = "test\0";
+		//char sz[] = "test\0";
 		//while(1)
-		{
+		/*{
 			SOCKADDR_IN bcast;
 			bcast.sin_family = AF_INET;
 			bcast.sin_addr.S_un.S_addr = inet_addr(MCAST_ADDR);
 			bcast.sin_port = htons(MCAST_PORT);
-			::sendto(s,sz,strlen(sz),0,(sockaddr*)&bcast,sizeof(bcast));
-		}
+			::sendto(s,sz,512,0,(sockaddr*)&bcast,sizeof(bcast));
+		}*/
+		count ++;
 	}
 	return 0;
 }
